9a770b56dbe5b7074c66239762d55061f7d6e5c7
palman2003 <palayilmanav@gmail.com>
2023-09-23 17:10:29 +0530

    created the routes and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Home from './components/Home'
+import User from './components/User'
+import Product from './components/Products'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   
   return (
-    <>
-   <Home/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/user' element={<User/>}/>
+      <Route path='/products' element={<Product/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import "../styles/Home.scss"
 
 const Home = () => {
   return (
-    <div className="Homepage">Home</div>
+    <div className="Homepage">Home aa</div>
   )
 }
 
