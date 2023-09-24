768cb0599cfd9740c68ad0d613dcd7cc30ca64c4
palman2003 <palayilmanav@gmail.com>
2023-09-24 15:40:54 +0530

    added the logo and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,52 @@
 import Home from './components/Home'
 import User from './components/User'
 import Product from './components/Products'
+import Navbar from './components/Navbar'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+import Menu from './components/Menu'
+import './styles/global.scss'
+//import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
+
+  const Layout=()=>{
+    return(
+      <div className='page'>
+        <Navbar/>
+        <div className='innerpage'>
+          <div className="menu"><Menu/></div>
+          <div className="content"><Outlet/></div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+  const router=createBrowserRouter([
+    {
+     path:'/',
+     element:<Layout/>,
+     children:[
+      {
+        path:'/',
+        element:<Home/>,
+      },
+      {
+        path:'user',
+        element:<User/>,
+      },
+      {
+        path:'product',
+        element:<Product/>,
+      },
+
+    ]
+    },
+  ]);
   
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/user' element={<User/>}/>
-      <Route path='/products' element={<Product/>}/>
-    </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div className='main'>
+      <div className="logo">
+        <img src="src/assets/logo.png" alt="react" width={75} height={75}/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
